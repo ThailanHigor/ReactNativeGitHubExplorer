1a97495b22f114bc15276fec08d91dcd1c86466e
Thailan <thailan-higor@hotmail.com>
2018-12-31 19:59:32 -0200

    Busca a api do github

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -1,33 +1,75 @@
-import React from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, { Component } from 'react';
+import { NavigationActions, StackActions } from 'react-navigation';
+import { View, Text, TextInput, TouchableOpacity, StatusBar } from 'react-native';
+import PropTypes from 'prop-types';
+import api from 'services/api';
 
 import styles from './styles';
 
-const Welcome = () => (
-  <View style={styles.container}>
-    <Text style={styles.title}> Bem-vindo</Text>
-    <Text style={styles.text}>
-      Para continuar, precisamos que você informe seu usuário no GitHub
-    </Text>
+export default class Welcome extends Component {
+  static navigationOptions = { header: null };
 
-    <View style={styles.form}>
-      <TextInput
-        style={styles.input}
-        autoCapitalize="none"
-        autoCorrect={false}
-        placeholder="Digite seu usuário"
-        underlineColorAndroid="rgba(0,0,0,0)"
-      />
+  static propTypes = {
+    navigation: PropTypes.shape({
+      dispatch: PropTypes.func,
+    }).isRequired,
+  };
 
-      <TouchableOpacity style={styles.button} onPress={() => {}}>
-        <Text style={styles.buttonText}>Prosseguir</Text>
-      </TouchableOpacity>
+  state = {
+    username: '',
+  }
 
+  checkUserExists = async (username) => {
+    const user = await api.get(`/users/${username}`);
+    return user;
+  }
 
-    </View>
+  singIn = async () => {
+    const { username } = this.state;
 
-  </View>
-);
-export default Welcome;
+    if (username.length === 0) return;
 
-Welcome.navigationOptions = { header: null };
+    try {
+      await this.checkUserExists(username);
+      const resetAction = StackActions.reset({
+        index: 0,
+        actions: [
+          NavigationActions.navigate({ routeName: 'User' }),
+        ],
+      });
+
+      this.props.navigation.dispatch(resetAction);
+    } catch (err) {
+      //erro
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Bem-vindo</Text>
+        <Text style={styles.text}>
+          Para continuar, precisamos que você informe seu usuário no GitHub
+        </Text>
+
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Digite seu usuário"
+            underlineColorAndroid="rgba(0,0,0,0)"
+            value={this.state.username}
+            onChangeText={username => this.setState({ username })}
+          />
+
+          <TouchableOpacity style={styles.button} onPress={this.singIn}>
+            <Text style={styles.buttonText}>Prosseguir</Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
+    );
+  }
+}
