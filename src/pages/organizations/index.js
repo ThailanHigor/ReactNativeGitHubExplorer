bb138f619c25a480fa69517f7a932d56225f7347
Thailan <thailan-higor@hotmail.com>
2019-01-01 18:54:49 -0200

    Load More e App Finalizado

--- src/pages/organizations/index.js
+++ src/pages/organizations/index.js
@@ -24,6 +24,7 @@ export default class Organizations extends Component {
   state = {
     data: [],
     loading: true,
+    refreshing: false,
   };
 
   componentDidMount() {
@@ -31,10 +32,11 @@ export default class Organizations extends Component {
   }
 
   loadOrganizations = async () => {
+    this.setState({ refreshing: true });
     const username = await AsyncStorage.getItem('@Githuber:username');
     const response = await api.get(`/users/${username}/orgs`);
 
-    this.setState({ data: response.data, loading: false, });
+    this.setState({ data: response.data, loading: false, refreshing: false, });
   };
 
   renderListItem = ({ item }) => (
@@ -48,6 +50,8 @@ export default class Organizations extends Component {
       renderItem={this.renderListItem}
       numColumns={2}
       columnWrapperStyle={styles.columnContainer}
+      onRefresh={this.loadOrganizations}
+      refreshing={this.state.refreshing}
     />
   );
 
